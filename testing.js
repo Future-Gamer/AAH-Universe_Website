a264fd54c185c80fbbf088c51cad3193e61e3faf
Arpan Parekh <arpanparekh06@gmail.com>
2025-05-17 15:34:14 +0530

    deleted testing files

--- testing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('season-dropdown').addEventListener('change', function() {
-          var selectedSeason = this.value;
-          document.querySelectorAll('.episode-list').forEach(function(list) {
-              list.style.display = list.id === selectedSeason ? 'block' : 'none';
-          });
-      });
-      
